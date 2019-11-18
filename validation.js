7ba1c4c55868ccb4f695c42b6c2536576a381a1f
Taleb_84 <taleb_n1@hotmail.com>
2019-11-18 18:46:25 +0100

    modify index.html and validation.js
    add password field to the view, add validation to the password.

--- validation.js
+++ validation.js
@@ -1,8 +1,18 @@
 function validateForm () {
-    const regName = /^[A-Za-z\s]+$/;
     let Name = document.forms["myForm"]["name"].value;
-    if (Name == "" || Name.length < 2 || regName.test(Name.value)) {
-        alert ("Please enter a valid name");
+    let Password = document.forms["myForm"]["password"].value;
+     if (Name == "") {
+        alert("Please enter your name");
+        return false;
+    } else if (Name.length < 2) {
+        alert ("name should be at lest 2 characters");
+        return false;
+    }
+    if (Password == ""){
+        alert("Please enter your password");
+        return false;
+    } else if (Password.length < 2) {
+        alert ("password should be at least 2 characters");
         return false;
     }
 }
